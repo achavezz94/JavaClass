63968501260e5798053ed81d8add382cd6130ec6
achavezz94 <achavezz94@outlook.com>
2018-02-03 23:51:43 -0500

    Added value counter
    Added countArray to count the number of times the value for n shows up.

--- HW1.java
+++ HW1.java
@@ -1,21 +1,41 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
 import java.util.Random;
+import java.util.Arrays;
 
+/**
+ *
+ * @author Alexis Chavez
+ */
+public class JavaApplication1 {
 
-public class HW1 {
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic hereimport java.util.Random;
+
         Random rand = new Random();
-        int count = 0;
-
-
-        while (count <= 1000){
-            double n = rand.nextInt(9) + 0;
-            System.out.println("Count = "+ count + " Random #: " + n);
-            count += 1;
+        int numarray[] = new int[10001];
+        for (int i = 0; i < numarray.length; i++){
+            double n = rand.nextInt(10) + 0;
+            numarray[i] = (int) n;
         }
-
+        
+        Arrays.sort(numarray);
+        int countArray[] = new int[numarray[numarray.length-1]+1];
+        for(int i:numarray){
+            countArray[i] = countArray[i]+1;
+        }
+        for (int i=0;i<countArray.length;i++){
+            System.out.println(i+" : " + countArray[i]);
+        }
     }
-
 }
+
+    
+
