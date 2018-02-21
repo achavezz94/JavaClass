75a11d3015f1996df90374ebbace24b3795eef11
achavezz94 <achavezz94@outlook.com>
2018-02-20 21:17:18 -0500

    Update Homework2B.java
    created loop for game

--- Homework2B.java
+++ Homework2B.java
@@ -1,20 +1,32 @@
 
+package TicTacToe;
 import java.util.*;
+
+
 
 public class Homework2B {
 
   public static void main(String[] args){
+      
     TicTacToe game = new TicTacToe();
-    game.isBoardFull();
-    if (isBoardFull == false){
-      game.printBoard();
-      Scanner sc = new Scanner(System.in);
-      int row = sc.nextInt();
-      int col = sc.nextInt();
-      game.placeMark(row,col);
-      game.printBoard();
+    Scanner sc = new Scanner(System.in);
+    game.initializeBoard();
+    System.out.println("Get ready to play.");
+    
+    while(true){
+       game.printBoard();
+       System.out.print("Choose the row: ");
+       int row = sc.nextInt();
+       System.out.print("Choose the column: ");
+       int col = sc.nextInt();
+       game.placeMark(row, col);
+       game.checkForWin();
+       game.changePlayer();
+       game.printBoard();
+       game.isBoardFull();
     }
-
+    
+    
   }
 
 }
@@ -62,19 +74,56 @@ class TicTacToe {
     }
     return isFull;
 }
+  public boolean checkForWin() {
+        return (checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin());
+    }
+	
+	
+    private boolean checkRowsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[i][0], board[i][1], board[i][2]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+	
+	
+    
+    private boolean checkColumnsForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[0][i], board[1][i], board[2][i]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+	
 
-public void changePlayer() {
-     if (currentPlayerMark == 'x') {
-         currentPlayerMark = 'o';
-     }
-     else {
-         currentPlayerMark = 'x';
-     }
- }
+    private boolean checkDiagonalsForWin() {
+        return ((checkRowCol(board[0][0], board[1][1], board[2][2]) == true) || (checkRowCol(board[0][2], board[1][1], board[2][0]) == true));
+    }
+	
+	
+    
+    private boolean checkRowCol(char c1, char c2, char c3) {
+        return ((c1 != '-') && (c1 == c2) && (c2 == c3));
+    }
+	
+	
+    
+    public void changePlayer() {
+        if (currentPlayerMark == 'x') {
+            currentPlayerMark = 'o';
+        }
+        else {
+            currentPlayerMark = 'x';
+        }
+    }
 
  public boolean placeMark(int row, int col) {
 
-      // Make sure that row and column are in bounds of the board.
+    
       if ((row >= 0) && (row < 3)) {
           if ((col >= 0) && (col < 3)) {
               if (board[row][col] == '-') {
